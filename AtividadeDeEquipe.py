fe1dd5159a8fe91e684afb68df543bca0b4a9236
HERBETON25 <herbeton25@gmail.com>
2024-08-06 22:52:49 -0300

    Atividade_While

--- AtividadeDeEquipe.py
+++ AtividadeDeEquipe.py
@@ -1,10 +1,18 @@
 import random
+cont = 1
 while True:
-    numero_Digitado = int(input('Digite um numero: '))
+    
+    numero_Digitado = int(input('Digite um numero de 1 a 10: '))
     numero_aleatorio = random.randint(1, 10)
     if numero_Digitado == numero_aleatorio:
-        print(f'Você Digitou ({numero_Digitado}) e o numero sorteado foi ({numero_aleatorio}) então você acertou.')
+        print(f'Você Digitou: ({numero_Digitado}) e o numero sorteado foi: ({numero_aleatorio}) Parabéns você acertou.')
         break
+    elif cont == 3:
+        print(f'Você Digitou: ({numero_Digitado}) e o numero sorteado foi: ({numero_aleatorio})')
+        print(f'Você atingio as três tentativas:Não foi dessa vez')
+        break
+        
     else:
-        print(f'Você Digitou ({numero_Digitado}) e o numero sorteado foi ({numero_aleatorio}) éntão você errou. Digite novamente!')
-print('Você acertou então o programa encerrou!')
+        print(f'Você Digitou: ({numero_Digitado}) e o numero sorteado foi: ({numero_aleatorio}) você errou. Vamos tentar mais uma Vez!')
+        cont += 1
+        
