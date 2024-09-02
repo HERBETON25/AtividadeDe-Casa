2395c3612fdd47403c51bfcdc3a2ca950c9ce976
HERBETON25 <herbeton25@gmail.com>
2024-09-02 19:14:46 -0300

    provaEstruturaDeRepeticaoFor

--- AtividadeDeEquipe.py
+++ AtividadeDeEquipe.py
@@ -1,18 +1,13 @@
-import random
-cont = 1
-while True:
-    
-    numero_Digitado = int(input('Digite um numero de 1 a 10: '))
-    numero_aleatorio = random.randint(1, 10)
-    if numero_Digitado == numero_aleatorio:
-        print(f'Você Digitou: ({numero_Digitado}) e o numero sorteado foi: ({numero_aleatorio}) Parabéns você acertou.')
-        break
-    elif cont == 3:
-        print(f'Você Digitou: ({numero_Digitado}) e o numero sorteado foi: ({numero_aleatorio})')
-        print(f'Você atingio as três tentativas:Não foi dessa vez')
-        break
-        
-    else:
-        print(f'Você Digitou: ({numero_Digitado}) e o numero sorteado foi: ({numero_aleatorio}) você errou. Vamos tentar mais uma Vez!')
-        cont += 1
-        
+inicio = int(input("Digite o início do intervalo: ")) 
+fim = int(input("Digite o fim do intervalo: ")) 
+
+somaPares = 0 
+
+for numero in range(inicio, fim + 1):
+    if numero % 2 == 0: 
+        somaPares += numero
+
+if somaPares == 0:
+    print("Não há números pares no intervalo.")
+else:
+    print(f"A soma dos números pares no intervalo é: {somaPares}")
